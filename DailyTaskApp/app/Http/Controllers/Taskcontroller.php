99cd50110744d6d24df902c3f43cea59c078d67f
sanju2 <lsanjeewa947@gmail.com>
2019-09-18 12:46:24 +0530

    Simple daily task application.Its make using laravel,mysql

---
+++ DailyTaskApp/app/Http/Controllers/Taskcontroller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class Taskcontroller extends Controller
+{
+    public function store(Request $request){
+       
+       $task=new Task;
+
+        $this->validate($request,[
+            'task'=>'required|max:100|min:5',
+        ]);
+
+       $task->task=$request->task;
+       $task->save();
+       $data=Task::all();
+       //dd($data);
+       return view('tasks')->with('tasks',$data);
+       // dd($request->all());
+
+    }
+    public function UpdateTaskAsCompleted($id){
+        $task=Task::find($id);
+        $task->iscompleted=1;
+        $task->save();
+        return redirect()->back();
+    }
+
+    public function UpdateTaskAsNotCompleted($id){
+        $task=Task::find($id);
+        $task->iscompleted=0;
+        $task->save();
+        return redirect()->back();
+    }
+    
+
+}
